41c95d428e88a4ec33558188cfc038b4ad7fb51a
Preston Carman <preston.carman@wallawalla.edu>
2024-05-01 14:55:03 -0700

    Adding starter dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM gitlab.cs.wallawalla.edu:5050/cs_department/docker-images/pandoc_latex:latest
+COPY renderLatexFile.sh /app/renderLatexFile.sh
+ENTRYPOINT ["/app/renderLatexFile.sh"]
+WORKDIR /app/tex
---
+++ renderLatexFile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+    
+# make sure we have a tex file as first argument
+if [ "$1" == "" ]; then
+  echo "Usage: renderLatexFile.sh fileprefix"
+  exit
+else 
+  if [ ! -e "$1.tex" ]; then
+    echo "File $1.tex not found"
+    exit
+  fi
+fi
+
+echo "Generating $1.pdf"
+echo "\n\n============ Generating $1.pdf =============\n\n" >> generator.log
+
+fbase=$(basename -- "$1");
+fname="${fbase%.*}"
+
+# generate the pdf file
+pdflatex --shell-escape -synctex=1 -interaction=nonstopmode $1.tex >> generator.log
+pdflatex --shell-escape -synctex=1 -interaction=nonstopmode $1.tex >> generator.log
+
+# clean up
+rm -f "${fname}".aux "${fname}".fdb_latexmk "${fname}".fls "${fname}".log "${fname}".nav "${fname}".out "${fname}".snm "${fname}".synctex.gz "${fname}".toc
+
