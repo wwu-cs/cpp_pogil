b22b57b628b0458b539311f74872add7f1cce876
Jaime Garcia <jagarcia767.jr@gmail.com>
2025-11-20 15:17:34 -0800

    Adding source files and pdf

---
+++ CS1/01_introduction/src/activity01c.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Your name is " << name;
+}
