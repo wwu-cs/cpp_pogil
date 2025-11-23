b54e1bb96b877d8289d9ac5a23e92780e4f4a7bb
Jaime Garcia <jagarcia767.jr@gmail.com>
2025-11-23 13:56:14 -0800

    Removing spaces around parentheses in activity05b.cpp

--- CS1/05_boolean/src/activity05b.cpp
+++ CS1/05_boolean/src/activity05b.cpp
@@ -9,6 +9,6 @@ int main() {
   cout << "Line 8:  " << (a == b) << endl;
   cout << "Line 9:  " << (a != b) << endl;
   cout << "Line 10: " << (a = b) << endl;
-  cout << "Line 11: " << ( (a > c) || ((b / c) == 1) ) << endl;
-  cout << "Line 12: " << ( (a > c) && ((b / c) == 1) ) << endl;
+  cout << "Line 11: " << ((a > c) || ((b / c) == 1)) << endl;
+  cout << "Line 12: " << ((a > c) && ((b / c) == 1)) << endl;
 }
