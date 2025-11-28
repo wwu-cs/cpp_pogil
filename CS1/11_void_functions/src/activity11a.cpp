5d75f38f76c16963abce62006242c32fef6b6a65
Jaime Garcia <jagarcia767.jr@gmail.com>
2025-11-28 11:41:33 -0800

    Chapter 11

---
+++ CS1/11_void_functions/src/activity11a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void printMessage() {
+  cout << "Welcome to C++." << endl;
+  cout << "Learn the power of functions!" << endl;
+}
+
+int main() {
+  cout << "Hello Programmer!" << endl;
+  printMessage();     // function call
+}
---
+++ CS1/11_void_functions/src/activity11c.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// first function
+void printSum(int num1, int num2) {
+  cout << num1 << " + " << num2 << " = " << (num1+num2) << endl;
+}
+// second function
+void printDifference(int num1, int num2) {
+  cout << num1 << " - " << num2 << " = " << (num1-num2) << endl;
+}
+// main program
+int main() {
+  // define variables
+  int firstNumber, secondNumber;
+  char operation;
+  // collect user input
+  cout << "Enter a number between 1 and 10: ";
+  cin >> firstNumber;
+  cout << "Enter another number between 1 and 10: ";
+  cin >> secondNumber;
+  cout << "Enter a '+' to add or a '-' to subtract: ";
+  cin >> operation;
+  // decide which function to call
+  if (operation == '+') {
+    printSum(firstNumber,secondNumber);
+  } else if (operation == '-') {
+    printDifference(firstNumber,secondNumber);
+  } else {
+    cout << "Invalid Operation" << endl;
+  }  
+}
